3e4c600268cabd8e9959d23b6e41650db8c31964
JafetHou <jafethou2005@gmail.com>
2024-02-02 15:48:17 -0600

    opcion crear

--- src/lab3p2_jafethou/Lab3P2_JafetHou.java
+++ src/lab3p2_jafethou/Lab3P2_JafetHou.java
@@ -1,20 +1,82 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lab3p2_jafethou;
 
-/**
- *
- * @author zenot
- */
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Lab3P2_JafetHou {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner lea = new Scanner (System.in);
+    static Scanner leer = new Scanner (System.in);
+    static Scanner scanner = new Scanner (System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        ArrayList <Pokemon> pokemones  = new ArrayList();
+        
+        boolean seguir = true;
+        
+        while(seguir){
+            
+            System.out.println("MENU\n"
+                    + "1) Crear pokemon\n"
+                    + "2) Crear pokebola\n"
+                    + "3) Listar pokemon\n"
+                    + "4) Eliminar pokemon\n"
+                    + "5) Capturar pokemon\n"
+                    + "6) Modificar pokemon\n"
+                    + "Ingrese [7] para salir: ");
+            int opc = lea.nextInt();
+            
+            switch(opc){
+                
+                case 1:{
+                    
+                    crear(pokemones);
+                    
+                    break;
+                }
+                case 7:{
+                    seguir = false;
+                    break;
+                }
+            }
+            
+        }
+        
+        
+    }
+    public static ArrayList crear(ArrayList <Pokemon> pokemones){
+        
+        System.out.println("Que tipo de pokemon desea agregar\n"
+                + "1) Fire\n"
+                + "2) Water\n"
+                + "3) Grass\n");
+        
+        int num = lea.nextInt();
+        
+        if(num == 1){
+            String tipo = "Fire";
+        }else if(num == 2){
+            String tipo = "Water";
+        }else if(num == 3){
+            String tipo = "Grass";
+        }else{
+            System.out.println("Numero incorrecto ingrese nuevamente: ");
+            crear(pokemones);
+        }
+        
+        System.out.println("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
+        
+        System.out.println("Ingrese numero en la pokedex: ");
+        int pokedex = lea.nextInt();
+        
+        System.out.println("Ingrese la naturaleza del pokemon: ");
+        String naturaleza = scanner.nextLine();
+        
+        pokemones.add((Pokemon) new Pokemon(nombre, nombre, pokedex, naturaleza, false, ""));
+        
+    return pokemones;
     }
     
 }
---
+++ src/lab3p2_jafethou/Pokemon.java
@@ -0,0 +1,78 @@
+
+package lab3p2_jafethou;
+
+
+public class Pokemon {
+    
+    String tipo;
+    String nombre;
+    int pokedex;
+    String naturaleza;
+    boolean atrapado;
+    String pokebola;
+
+    public Pokemon(String tipo, String nombre, int pokedex, String naturaleza, boolean atrapado, String pokebola) {
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.pokedex = pokedex;
+        this.naturaleza = naturaleza;
+        this.atrapado = atrapado;
+        this.pokebola = pokebola;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPokedex() {
+        return pokedex;
+    }
+
+    public String getNaturaleza() {
+        return naturaleza;
+    }
+
+    public boolean isAtrapado() {
+        return atrapado;
+    }
+
+    public String getPokebola() {
+        return pokebola;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPokedex(int pokedex) {
+        this.pokedex = pokedex;
+    }
+
+    public void setNaturaleza(String naturaleza) {
+        this.naturaleza = naturaleza;
+    }
+
+    public void setAtrapado(boolean atrapado) {
+        this.atrapado = atrapado;
+    }
+
+    public void setPokebola(String pokebola) {
+        this.pokebola = pokebola;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon{" + " nombre: " + nombre + ", tipo: " + tipo + ", pokedex: " + pokedex + ", naturaleza: " + naturaleza + ", atrapado: " + atrapado + ", pokebola: " + pokebola + '}';
+    }
+    
+    
+    
+}
